fd485056f9d42e26494d45b3e519dcebf9c72e1e
dave4th <mail4davide@gmail.com>
2014-12-27 12:00:39 +0100

    Visualizzazione messaggi

--- var/www/cgi-bin/messageread.py
+++ var/www/cgi-bin/messageread.py
@@ -58,7 +58,8 @@ if len(Msg) != 0:       # Se ci sono messaggi da visualizzare ..
         print ("</tr>")
     print ("</table>")
     
-    print ("<hr/>")
+    print ("<br>")
+    print ("<br>")
 
 Msg = (MyDB.keys("new:"+RedisKey+"*"))  # new:msg:*
 if len(Msg) != 0:
@@ -96,11 +97,41 @@ if len(Msg) != 0:
         print ("</tr>")
     print ("</table>")
     
+    print ("<br>")
+    
+    print (mhl.MyActionForm("/cgi-bin/messagewrite.py","POST"))
+    print ("<table>")
+    print ("<tr>")
+    print ("<td>")
+    print ("<b>Spostare messaggi \"inviati/in invio\" in \"vecchi messaggi\" ?   </b>")
+    print ("</td>")
+    print ("<td>")
+    print (mhl.MyCheckboxForm("key","new:"+RedisKey+"*"))
+    print ("</td>")
+    print ("</tr>")
+    print ("<tr>")
+    print ("<td colspan=\"4\">")
     print ("<hr/>")
+    print ("</td>")
+    print ("</tr>")
+    print ("<tr>")
+    print ("<td>")
+    print ("</td>")
+    print ("<td>")
+    print (mhl.MyButtonForm("submit","Submit"))
+    print ("</td>")
+    print ("</tr>")
+    print ("</table>")
+    print (mhl.MyEndForm())
+    
+    print ("<br>")
+    print ("<br>")
 
 Msg = (MyDB.keys("old:"+RedisKey+"*"))  # old:msg:*
+Msg = sorted(Msg)
+#Msg = sorted(Msg,key=lambda Msg: Msg[4], reverse=True)
 if len(Msg) != 0:
-    print ("<h3>Vecchi messaggi<h3>")
+    print ("<h3>Vecchi messaggi (ordinati per tipo di allarme)</h3>")
     print ("<table border=\"1\" cellspacing=\"0\" cellpadding=\"3\">")
     print ("<tr>")
     print ("<td>")
@@ -134,34 +165,7 @@ if len(Msg) != 0:
         print ("</tr>")
     print ("</table>")
     
-    print ("<hr/>")
     print ("<br>")
-
-Msg = (MyDB.keys("new:"+RedisKey+"*"))  # msg:*
-if len(Msg) != 0:
-    print (mhl.MyActionForm("/cgi-bin/messagewrite.py","POST"))
-    print ("<table>")
-    print ("<tr>")
-    print ("<td>")
-    print ("<b>Spostare messaggi \"inviati/in invio\" in \"vecchi messaggi\" ?   </b>")
-    print ("</td>")
-    print ("<td>")
-    print (mhl.MyCheckboxForm("key","new:"+RedisKey+"*"))
-    print ("</td>")
-    print ("</tr>")
-    print ("<tr>")
-    print ("<td colspan=\"4\">")
-    print ("<hr/>")
-    print ("</td>")
-    print ("</tr>")
-    print ("<tr>")
-    print ("<td>")
-    print ("</td>")
-    print ("<td>")
-    print (mhl.MyButtonForm("submit","Submit"))
-    print ("</td>")
-    print ("</tr>")
-    print ("</table>")
-    print (mhl.MyEndForm())
+    print ("<br>")
 
 print (mhl.MyHtmlBottom())
